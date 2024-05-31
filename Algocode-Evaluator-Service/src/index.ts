4fa48c6053fd82c5b0bccf00310d68d6b9b149a2
Abhiadi12 <abhisekmishra172@gmail.com>
2024-05-31 19:39:30 +0530

    Day-15: setup bullborad-express route

--- Algocode-Evaluator-Service/src/index.ts
+++ Algocode-Evaluator-Service/src/index.ts
@@ -4,10 +4,13 @@ import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
+import bullBoardAdapter from "./config/bullBoardConfig";
 
 const app: Express = express();
 
 app.use("/api", apiRouter);
+app.use("bullmq-ui", bullBoardAdapter.getRouter());
+
 app.get("/", (__, res) => {
   return res.send("hello express ts server abhisek-bidisha");
 });
@@ -15,11 +18,7 @@ app.get("/", (__, res) => {
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
   SampleWorker("SampleQueue");
-
-  sampleQueueProducer("SampleJob", {
-    name: "Abhisek",
-    company: "mindfire",
-    position: "SDE 1",
-    location: "Banglore",
-  });
+  console.log(
+    `Bullboard dashboard running on: http://localhost:${serverConfig.PORT}/bullmq-ui`
+  );
 });
